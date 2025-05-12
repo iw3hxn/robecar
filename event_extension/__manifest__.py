36c855c778845c1b107fa6c508e0ebf45a5c3eac
carlo <carlo.vettore@didotech.com>
2025-05-12 12:54:33 +0200

    Enhance event registration report formatting and functionality
    Added global count tracking and improved table styling with borders and dynamic fields. Introduced support for displaying table descriptions and refined header formatting for better readability. Updated manifest version to reflect these changes.

--- event_extension/__manifest__.py
+++ event_extension/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Event Registration Extension',
-    'version': '18.0.0.2.0',
+    'version': '18.0.0.2.1',
     'author': 'Carlo Vettore',
     'category': 'Events',
     'depends': ['event'],
